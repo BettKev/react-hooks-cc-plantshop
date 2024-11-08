c6979f2ae70c90e7e810bd502bea7ed76773d64e
Kevin Bett <prodigy4614@gmail.com>
2024-11-08 12:47:22 +0300

    fixed bug

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,43 +1,38 @@
+// NewPlantForm.js
 import React, { useState } from "react";
 
-function NewPlantForm() {
+function NewPlantForm({ onAddPlant }) {
   const [formData, setFormData] = useState({
     name: "",
     image: "",
     price: "",
   });
 
-  function handleChange(e){
-    const {name, value} = e.target;
-    setFormData({...formData, [name]: value});
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
   }
 
-  function handleSubmit (e){
+  function handleSubmit(e) {
     e.preventDefault();
     fetch("https://phase2-week2-plantsy-code-challenge.onrender.com/plants", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-
-
-
-
-
-
-
       body: JSON.stringify(formData),
     })
-    .then((response)=>response.json())
-    .then((newPlant)=>onAddPlant(newPlant));
+      .then((response) => response.json())
+      .then((newPlant) => onAddPlant(newPlant));
   }
+
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="/images/image_name" />
-        <input type="number" name="price" step="0.01" placeholder="Price" />
+        <input type="text" name="name" placeholder="Plant name" onChange={handleChange} />
+        <input type="text" name="image" placeholder="Image URL" onChange={handleChange} />
+        <input type="number" name="price" step="0.01" placeholder="Price" onChange={handleChange} />
         <button type="submit">Add Plant</button>
       </form>
     </div>
