eea2bb711cc14c3555de01dbda5b3afecc38a219
Kevin Bett <prodigy4614@gmail.com>
2024-11-08 12:42:32 +0300

    update form to handle adding new plant

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function NewPlantForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    image: "",
+    price: "",
+  });
+
+  function handleChange(e){
+    const {name, value} = e.target;
+    setFormData({...formData, [name]: value});
+  }
+
+  function handleSubmit (e){
+    e.preventDefault();
+    fetch("https://phase2-week2-plantsy-code-challenge.onrender.com/plants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+
+
+
+
+
+
+      body: JSON.stringify(formData),
+    })
+    .then((response)=>response.json())
+    .then((newPlant)=>onAddPlant(newPlant));
+  }
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
-        <input type="text" name="image" placeholder="Image URL" />
+        <input type="text" name="image" placeholder="/images/image_name" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
         <button type="submit">Add Plant</button>
       </form>
