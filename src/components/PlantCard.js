ea2b865cc3ec3b2a7f860733e1f81619db2151be
Kevin Bett <prodigy4614@gmail.com>
2024-11-08 12:18:08 +0300

    fixed image element display bug

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -6,7 +6,7 @@ function PlantCard({ plant }) {
 
   return (
     <li className="card" data-testid="plant-item">
-      <img src={plant.image} alt={plant.name} />
+      <img src={process.env.PUBLIC_URL + plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
       <p>Price: ${plant.price}</p>
       <button onClick={() => setIsInStock(!isInStock)} className={isInStock ? "primary" : ""}>
