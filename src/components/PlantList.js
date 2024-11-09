86ac60d68bb9f4734853eac6ebadd6ec91548fc6
Kevin Bett <prodigy4614@gmail.com>
2024-11-09 22:32:29 +0300

    fixed filter bug

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -3,6 +3,7 @@ import React from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList({ plants }) {
+  
   return (
     <ul className="cards">
       {plants.map((plant) => (
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -6,6 +6,7 @@ import Search from "./Search";
 
 function PlantPage() {
   const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     fetch("https://phase2-week2-plantsy-code-challenge.onrender.com/plants")
@@ -17,11 +18,15 @@ function PlantPage() {
     setPlants([...plants, newPlant]);
   }
 
+  const filteredPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <main>
       <NewPlantForm onAddPlant={handleAddPlant} />
-      <Search />
-      <PlantList plants={plants} />
+      <Search searchTerm={searchTerm} onSearch={setSearchTerm} />
+      <PlantList plants={filteredPlants} />
     </main>
   );
 }
